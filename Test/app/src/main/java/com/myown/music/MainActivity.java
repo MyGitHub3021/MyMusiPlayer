ff58f8f7d562ba230503e80aef8ac9d14c7b4ff4
bilage <786261968@qq.com>
2016-05-13 08:08:39 +0800

    完成布局

--- Test/app/src/main/java/com/myown/music/MainActivity.java
+++ Test/app/src/main/java/com/myown/music/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.test.test;
+package com.myown.music;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.test.test.R;
 
 
 public class MainActivity extends Activity {
